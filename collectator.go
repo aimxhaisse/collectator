65a17cf2ec995300e39c2f15d0b1e58afdccbfdf
s. rannou <mxs@sbrk.org>
2015-01-03 11:38:50 +0100

    Added argument parsing (--period) and main loop.

--- collectator.go
+++ collectator.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"time"
+	"fmt"
+)
+
+var (
+	periodSec = flag.Int("period", 1, "refresh period in seconds")
+)
+
+type Collectator struct {
+	periodSec int // period in second between each collect
+}
+
+func (c *Collectator) Run() {
+	for {
+		fmt.Printf("Hello World\n")
+		time.Sleep(time.Duration(c.periodSec) * time.Second)
+	}
+}
+
+func NewCollectator() *Collectator {
+	return &Collectator{
+		*periodSec,
+	}
+}
 
 func main() {
-	fmt.Printf("Hello world\n")
+	flag.Parse()
+	c := NewCollectator()
+	c.Run()
 }
