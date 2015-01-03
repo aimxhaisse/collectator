f65ad9cc6b4113de81fb2b1f74efc56c26a97043
s. rannou <mxs@sbrk.org>
2015-01-03 11:53:27 +0100

    Added collect of load average.

--- collectator.go
+++ collectator.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"flag"
-	"time"
 	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var (
@@ -11,19 +14,37 @@ var (
 )
 
 type Collectator struct {
-	periodSec int // period in second between each collect
+	periodSec int     // period in second between each collect
+	loadAvg   float64 // current load average
 }
 
 func (c *Collectator) Run() {
 	for {
-		fmt.Printf("Hello World\n")
+		c.refreshMetrics()
+		fmt.Printf("load_avg:%f\n", c.loadAvg)
 		time.Sleep(time.Duration(c.periodSec) * time.Second)
+	}
+}
+
+func (c *Collectator) refreshMetrics() {
+	c.refreshLoadAvg()
+}
+
+func (c *Collectator) refreshLoadAvg() {
+	buf, err := ioutil.ReadFile("/proc/loadavg")
+	if err == nil {
+		line := string(buf)
+		i := strings.IndexByte(line, ' ')
+		if i > 0 {
+			c.loadAvg, err = strconv.ParseFloat(line[:i], 64)
+		}
 	}
 }
 
 func NewCollectator() *Collectator {
 	return &Collectator{
 		*periodSec,
+		0,
 	}
 }
 
