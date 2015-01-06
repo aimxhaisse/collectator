9883fc17c016a07ffda50c7c65f2e58807ce3d0a
s. rannou <mxs@sbrk.org>
2015-01-06 11:40:14 +0100

    Added mem_active to reported metrics.

--- collectator.go
+++ collectator.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -16,18 +19,40 @@ var (
 type Collectator struct {
 	periodSec int     // period in second between each collect
 	loadAvg   float64 // current load average
+	memActive float64 // current active memory in kB
 }
 
 func (c *Collectator) Run() {
 	for {
 		c.refreshMetrics()
-		fmt.Printf("collectator> load_avg:%f\n", c.loadAvg)
+		fmt.Printf("collectator> load_avg:%f mem_active:%f\n", c.loadAvg, c.memActive)
 		time.Sleep(time.Duration(c.periodSec) * time.Second)
 	}
 }
 
 func (c *Collectator) refreshMetrics() {
 	c.refreshLoadAvg()
+	c.refreshMemory()
+}
+
+var (
+	re_mem_active = regexp.MustCompile("^Active:\\s+([0-9]+)\\s+kB$")
+)
+
+func (c *Collectator) refreshMemory() {
+	file, err := os.Open("/proc/meminfo")
+	if err == nil {
+		defer file.Close()
+		scan := bufio.NewScanner(file)
+		for scan.Scan() {
+			line := scan.Text()
+
+			if m := re_mem_active.FindStringSubmatch(line); m != nil {
+				fmt.Printf("match=%s\n", m[1])
+				c.memActive, err = strconv.ParseFloat(m[1], 64)
+			}
+		}
+	}
 }
 
 func (c *Collectator) refreshLoadAvg() {
@@ -44,6 +69,7 @@ func (c *Collectator) refreshLoadAvg() {
 func NewCollectator() *Collectator {
 	return &Collectator{
 		*periodSec,
+		0,
 		0,
 	}
 }
